668fb26069111b3188e0b6e2753f6ae14e78617c
Benjamin McLean <mclean.ben@gmail.com>
2019-11-23 13:53:56 -0600

    Added code of MS-DOS screen

--- Spatial.cs
+++ Spatial.cs
@@ -3,19 +3,97 @@ using System;
 
 public class Spatial : Godot.Spatial
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	private VirtualScreenText virtualScreenText = new VirtualScreenText();
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+	private DateTime now = DateTime.Now;
+	private Random random = new Random();
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public override void _Ready()
+	{
+		VisualServer.SetDefaultClearColor(Color.Color8(0, 0, 0, 255));
+
+		Camera camera = new Camera()
+		{
+			Current = true,
+		};
+		AddChild(camera);
+
+		WorldEnvironment worldEnvironment = new WorldEnvironment()
+		{
+			Environment = new Godot.Environment()
+			{
+				BackgroundColor = Color.Color8(85, 85, 85, 255),
+				BackgroundMode = Godot.Environment.BGMode.Color,
+			},
+		};
+		AddChild(worldEnvironment);
+
+		Viewport viewport = new Viewport()
+		{
+			Size = new Vector2(720, 400),
+			Disable3d = true,
+			RenderTargetClearMode = Viewport.ClearMode.OnlyNextFrame,
+			RenderTargetVFlip = true,
+		};
+		AddChild(viewport);
+		viewport.AddChild(new ColorRect()
+		{
+			Color = Color.Color8(0, 0, 0, 255),
+			RectSize = viewport.Size,
+		});
+
+		AddChild(new Sprite3D()
+		{
+			Texture = viewport.GetTexture(),
+			PixelSize = 0.00338666666f,
+			Scale = new Vector3(1f, 1.35f, 1f),
+			MaterialOverride = new SpatialMaterial()
+			{
+				FlagsUnshaded = true,
+				FlagsDoNotReceiveShadows = true,
+				FlagsDisableAmbientLight = true,
+				ParamsSpecularMode = SpatialMaterial.SpecularMode.Disabled,
+				ParamsCullMode = SpatialMaterial.CullMode.Disabled,
+				FlagsTransparent = false,
+			},
+			GlobalTransform = new Transform(Basis.Identity, new Vector3(0, 0, -2)),
+		});
+
+		BitmapFont font = new BitmapFont();
+		font.CreateFromFnt("Bm437_IBM_VGA9.fnt");
+		Theme theme = new Theme()
+		{
+			DefaultFont = font,
+		};
+
+		Label label = new Label()
+		{
+			Theme = theme,
+			RectPosition = new Vector2(0, 0),
+		};
+		label.Set("custom_constants/line_spacing", 0);
+		label.Set("custom_colors/font_color", Color.Color8(170, 170, 170, 255));
+		virtualScreenText.Label = label;
+		viewport.AddChild(label);
+	}
+
+	public override void _Process(float delta)
+	{
+		if (DateTime.Now - now > TimeSpan.FromSeconds(1))
+		{
+			now = DateTime.Now;
+			double trouble = random.NextDouble();
+			string message;
+			if (trouble < 1d / 3d)
+				message = "Short message ";
+			else if (trouble < 2d / 3d)
+				message = "Slightly longer message ";
+			else if (trouble < 0.9d)
+				message = "Really really long but not quite the longest message ";
+			else
+				message = "The Boy Bands Have Won, and All the Copyists and the Tribute Bands and the TV Talent Show Producers Have Won, If We Allow Our Culture to Be Shaped by Mimicry, Whether from Lack of Ideas or from Exaggerated Respect. You Should Never Try to Freeze Culture. What You Can Do Is Recycle That Culture. Take Your Older Brother's Hand-Me-Down Jacket and Re-Style It, Re-Fashion It to the Point Where It Becomes Your Own. But Don't Just Regurgitate Creative History, or Hold Art and Music and Literature as Fixed, Untouchable and Kept Under Glass. The People Who Try to 'Guard' Any Particular Form of Music Are, Like the Copyists and Manufactured Bands, Doing It the Worst Disservice, \nBecause the Only Thing That You Can Do to Music That Will Damage It Is Not Change It, Not Make It Your Own. Because Then It Dies, Then It's Over, Then It's Done, and the Boy Bands Have Won ";
+
+			virtualScreenText.WriteLine(message + now.ToString());
+		}
+	}
 }
---
+++ VirtualScreenText.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class VirtualScreenText
+{
+    private readonly Queue<string> lines = new Queue<string>();
+
+    public string Text
+    {
+        get
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (string line in lines)
+                stringBuilder.Append(line).Append("\n");
+            return stringBuilder.ToString();
+        }
+        set
+        {
+            lines.Clear();
+            WriteLine(value);
+        }
+    }
+
+    private Godot.Label label;
+    public Godot.Label Label
+    {
+        get { return label; }
+        set
+        {
+            label = value;
+            if (label != null)
+                label.Text = Text;
+        }
+    }
+
+    public uint Height { get; set; } = 25;
+    public uint Width { get; set; } = 80;
+
+    public override string ToString()
+    {
+        return Text;
+    }
+
+    public VirtualScreenText CLS()
+    {
+        Text = string.Empty;
+        return this;
+    }
+
+    public VirtualScreenText WriteLine(string value)
+    {
+        foreach (string line in Wrap(value).Split('\n'))
+            lines.Enqueue(line);
+        if (Height > 0)
+            while (lines.Count() > Height)
+                lines.Dequeue();
+        if (Label != null)
+            Label.Text = Text;
+        return this;
+    }
+
+    public string Wrap(string value)
+    {
+        return Wrap(value, Width);
+    }
+
+    public static string Wrap(string value, uint width)
+    {
+        if (width <= 0)
+            return value;
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (string a in value.Split('\n'))
+            foreach (string b in ChunksUpto(a, width))
+                stringBuilder.Append(b).Append("\n");
+        return TrimLastCharacter(stringBuilder.ToString());
+    }
+
+    public static IEnumerable<string> ChunksUpto(string str, uint maxChunkSize)
+    {
+        for (int i = 0; i < str.Length; i += (int)maxChunkSize)
+            yield return str.Substring(i, Math.Min((int)maxChunkSize, str.Length - i));
+    }
+
+    public static string TrimLastCharacter(string str)
+    {
+        return string.IsNullOrEmpty(str) ? str : str.Substring(0, (str.Length - 1));
+    }
+}
